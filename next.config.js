b21b6cd5b4728f58121a9757d23205977f453def
john-twingate <yuyat8448@gmail.com>
2025-02-04 18:46:11 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      undici: 'node-fetch'
+    };
+    return config;
+  },
+  experimental: {
+    serverActions: true
+  }
 }
+
+module.exports = nextConfig
